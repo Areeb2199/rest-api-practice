812a4f22460eeda0aef4de92419452164640b6b3
Areeb Babar <areebnokia1@gmail.com>
2025-01-27 00:36:12 +0000

    Starter Commit

---
+++ src/test/java/com/learn_to_rest/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_to_rest.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
